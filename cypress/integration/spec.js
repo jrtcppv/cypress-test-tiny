8cfc150fcd052375f2f296932a0868c2bff240af
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2021-08-11 10:53:01 -0400

    Update filter so only submit type input is checked

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,7 +2,7 @@ describe('Test event listeners', () => {
   it('makes sure event listener works', () => {
     cy.visit('./index.html').wait(10000).then(() => {
       cy.get('text-input').find('input').type('asdf').then(() => {
-          cy.get('input').should('not.have.attr', 'disabled')
+          cy.get('input[type="submit"]').should('not.have.attr', 'disabled')
         });
       });
   });
